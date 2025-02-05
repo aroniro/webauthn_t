f19811f6fa9f53e0025b33f2e7c95190be3f4d06
Glitch (passkeys-codelab) <none>
2025-02-05 07:06:56 +0000

    🍏🍣 Checkpoint ./server.mjs:74558037/281

--- server.mjs
+++ server.mjs
@@ -145,10 +145,8 @@ app.get('/.well-known/apple-app-site-association', (req, res) => {
     }
   };
   
-  res.setHeader('Content-Type', 'application/json'); // MIME 타입 설정
-  res.send(JSON.stringify(assetlinks)); // JSON으로 변환 후 응답
-
-  // res.json(assetlinks);
+  res.type('application/json');
+  res.send(JSON.stringify(assetlinks));
 });
 
 app.get('/test', (req, res) => {
